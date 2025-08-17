62ea8d56e531fcb4f6b9fe5571b486c56ec8c681
rakibulhasan456 <rakibulhasan456@gmail.com>
2025-08-18 01:12:20 +0600

    third

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,10 +49,15 @@ function loadContent(page) {
   const content = document.getElementById("content");
   
   if (page === "home") {
-    content.innerHTML = `
-      <h2>Welcome to MFTech Helper</h2>
-      <p>Navigate to Menu for more shortcuts.</p>
-    `;
+  const content = document.getElementById("content");
+  content.innerHTML = `
+    <div class="home-wrap">
+      <h4 class="home-h4">Welcome to</h4>
+      <h1 class="home-h1">MFTech</h1>
+      <h2 class="home-h2">HELPER</h2>
+      <h5 class="home-h5">Navigate to Menu for more shortcuts.</h5>
+    </div>
+  `;
   } else if (page === "staffInfo") {
     content.innerHTML = `
       <iframe src="https://hrportal.brac.net/profile/StaffInfoIndividual.aspx"
@@ -93,6 +98,108 @@ Personal SIM Number (only if doesn’t have BRAC SIM):</pre>
       </div>
     `;
   }
+    else if (page === "createEmail") {
+  content.innerHTML = `
+    <div class="card">
+      <h3>Create New BRAC Email</h3>
+      <hr>
+      <p><b>To:</b> it-support@brac.net</p>
+      <p><b>CC:</b> mf.email@brac.net, Supervisors (if needed)</p>
+      <p><b>Subject:</b> Request to Create New Email ID (Branch Name - Code OR Area Name OR Region Name)</p>
+      <hr>
+      <p><b>Email Body:</b></p>
+      <div id="createEmailBody">
+        <p>Dear Concern:<br>
+        Please create a new Email ID for the User mentioned below:</p>
+
+        <div style="overflow-x: auto; max-width: 100%;">
+          <table cellspacing="0" cellpadding="5" 
+            style="border-collapse: collapse; font-family: Arial, sans-serif; font-size: 13px; min-width: 900px; text-align: center; border: 1px solid black; width: 100%;">
+            <thead>
+              <tr>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">SL</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">User Name</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">User PIN</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Designation<br>(Dabi/Progoti/BCUP/SCDP/NCDP)</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">User Grade</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Phone Number<br>(Personal/Official)</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Base Office Name & Code</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Area Name</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Region Name</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Division Name<br>(If Request for <b>RM (SAM)</b> Mail Id)</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Supervisor's Email<br>(If Request for AAM/ABM/ARM Mail Id)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td style="border: 1px solid black;">1.</td>
+                <td style="border: 1px solid black;">Example</td>
+                <td style="border: 1px solid black;">00000</td>
+                <td style="border: 1px solid black;">AAM (Progoti)</td>
+                <td style="border: 1px solid black;"></td>
+                <td style="border: 1px solid black;">017********</td>
+                <td style="border: 1px solid black;">Example-0000</td>
+                <td style="border: 1px solid black;">Example</td>
+                <td style="border: 1px solid black;">Example</td>
+                <td style="border: 1px solid black;">Example</td>
+                <td style="border: 1px solid black;">am.p.example@brac.net</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <button class="copy-btn" onclick="copyCreateEmail()">Copy Email Body</button>
+    </div>
+  `;
+}
+else if (page === "renameEmail") {
+  content.innerHTML = `
+    <div class="card">
+      <h3>Rename Email ID</h3>
+      <hr>
+      <p><b>To:</b> it-support@brac.net</p>
+      <p><b>CC:</b> mf.email@brac.net, User mail ID, Staff’s Supervisor mail ID, Yours’s Supervisor mail ID</p>
+      <p><b>Subject:</b> Request to rename Email ID (Branch name- Code Or Area Name Or Region Name)</p>
+      <hr>
+      <p><b>Email Body:</b></p>
+      <div id="renameEmailBody">
+        <p>Dear IT support:<br>
+        Greetings! For operational purposes (The reason for changing the name of the mentioned email id). The information are as follows:</p>
+
+        <div style="overflow-x: auto; max-width: 100%;">
+          <table cellspacing="0" cellpadding="5" 
+       style="border-collapse: collapse; font-family: Arial, sans-serif; font-size: 13px; table-layout: fixed; min-width: 700px; border: 1px solid black;">
+            <thead>
+              <tr>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">SL</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">User Name</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">User PIN</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">User Designation</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Phone Number</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Base Office Name & Code</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Present Email ID</th>
+                <th style="border: 1px solid black; padding: 5px; white-space: nowrap;">Proposal for Renaming Email Id As-</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td style="border: 1px solid black;">1</td>
+                <td style="border: 1px solid black;"></td>
+                <td style="border: 1px solid black;"></td>
+                <td style="border: 1px solid black;"></td>
+                <td style="border: 1px solid black;"></td>
+                <td style="border: 1px solid black;"></td>
+                <td style="border: 1px solid black;"></td>
+                <td style="border: 1px solid black;"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <button class="copy-btn" onclick="copyrenameEmail()">Copy Email Body</button>
+    </div>
+  `;
+}
 }
 
 // Copy Email Function
@@ -104,3 +211,49 @@ function copyEmail() {
     setTimeout(() => btn.textContent = "Copy Email Body", 2000);
   }).catch(err => console.error("Copy failed:", err));
 }
+
+
+// Copy create email function
+
+function copyCreateEmail() {
+  const range = document.createRange();
+  const emailBody = document.getElementById("createEmailBody");
+  range.selectNode(emailBody);
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  try {
+    document.execCommand("copy");
+    const btn = document.querySelector("button[onclick='copyCreateEmail()']");
+    btn.textContent = "Copied!";
+    setTimeout(() => btn.textContent = "Copy Email Body", 2000);
+  } catch (err) {
+    console.error("Copy failed:", err);
+  }
+  selection.removeAllRanges();
+}
+
+// Copy rename email function
+
+function copyrenameEmail() {
+  const range = document.createRange();
+  const emailBody = document.getElementById("renameEmailBody");
+  range.selectNode(emailBody);
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  try {
+    document.execCommand("copy");
+    const btn = document.querySelector("button[onclick='copyrenameEmail()']");
+    btn.textContent = "Copied!";
+    setTimeout(() => btn.textContent = "Copy Email Body", 2000);
+  } catch (err) {
+    console.error("Copy failed:", err);
+  }
+  selection.removeAllRanges();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadContent("home");
+});
+
